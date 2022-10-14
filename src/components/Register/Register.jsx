039d8c9f41d9aedaf443927b023dd746e639da2b
Homegirl101 <iamsharon067@gmail.com>
2022-10-14 14:31:52 -0700

    putting correct ajax link

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -32,7 +32,7 @@ class Register extends Component{
 
     onButtonClick = () => {
         console.log(this.state)
-        fetch("https://young-scrubland-15976.herokuapp.com/register", {
+        fetch("https://ancient-thicket-25925.herokuapp.com/register", {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/signin/signin.jsx
+++ src/components/signin/signin.jsx
@@ -21,7 +21,7 @@ class Signin extends Component {
     };
     onSubmit = () => {
         console.log(this.state);
-        fetch("https://young-scrubland-15976.herokuapp.com/signin", {
+        fetch("https://ancient-thicket-25925.herokuapp.com/signin", {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
